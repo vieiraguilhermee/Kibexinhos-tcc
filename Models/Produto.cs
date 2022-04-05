531f994333d385cc608adb9da137e52fd1a21673
vieiraguilhermee <100038792+vieiraguilhermee@users.noreply.github.com>
2022-04-05 13:14:21 -0300

    Alteração propriedades da entitdade produto

--- Models/Produto.cs
+++ Models/Produto.cs
@@ -20,6 +20,27 @@ namespace Kibexinhos.Models
         public string? Descricao { get; set; }
 
         [Required(ErrorMessage = "Este campo é obrigatório")]
+        [MaxLength(100, ErrorMessage = "Este campo deve conter entre 10 e 100 caracteres")]
+        [MinLength(10, ErrorMessage = "Este campo deve conter entre 10 e 100 caracteres")]
+        public string? InformacaoNutricional { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, double.MaxValue)]
+        public double Altura { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, double.MaxValue)]
+        public double Largura { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, double.MaxValue)]
+        public double Comprimento { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Range(0, double.MaxValue)]
+        public double Peso { get; set; }
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
         public double Preco { get; set; }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
